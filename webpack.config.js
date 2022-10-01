0748eed7ffda34ea12e179a201f93670060beceb
quan <304390323@qq.com>
2022-10-01 11:47:45 +0800

    init

---
+++ webpack.config.js
@@ -0,0 +1,28 @@
+// var HtmlWebpackPlugin = require('html-webpack-plugin');
+var path = require('path');
+
+module.exports = {
+  mode: 'development',
+//   devtool: 'inline-source-map',
+//    devServer: {
+//     contentBase: './dist',
+//   },
+  entry: './src/index.js',
+  output: {
+    path: path.resolve(__dirname, 'dist'),
+    filename: 'index.[contenthash].js'
+  },
+//  plugins: [new HtmlWebpackPlugin({
+//   title: 'quan',
+//   template: 'src/assets/index.html'
+// })],
+// module: {
+//   rules: [
+//     {
+//       test: /\.css$/i,
+//       use: ["style-loader", "css-loader"],
+//     },
+//   ],
+// },
+};
+

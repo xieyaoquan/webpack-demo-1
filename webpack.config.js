2c82abd4d239e1c488e3413432fad027039faf90
quan <304390323@qq.com>
2022-10-01 23:15:57 +0800

    update

--- webpack.config.js
+++ webpack.config.js
@@ -3,10 +3,10 @@ const path = require('path');
 
 module.exports = {
   mode: 'development',
-//   devtool: 'inline-source-map',
-//    devServer: {
-//     contentBase: './dist',
-//   },
+  devtool: 'inline-source-map',
+   devServer: {
+    contentBase: './dist',
+  },
   entry: './src/index.js',
   output: {
     path: path.resolve(__dirname, 'dist'),

61085ba445b2ddf4f491ba0dbc3602497e81c50f
quan <304390323@qq.com>
2022-10-06 22:13:32 +0800

    fix bug

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import x from './x.js'
 import png from './assets/1.png'
 
-// const div = document.getElementById('app')
+const div = document.getElementById('app')
 // div.innerHTML=`
 // <img src='${png}'>
 // `
